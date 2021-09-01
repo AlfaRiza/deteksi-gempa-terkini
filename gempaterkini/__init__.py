61421e84f7532fb0d0df46e5c8218faa213c27d8
AlfaRiza <malfariza45@gmail.com>
2021-09-02 06:57:45 +0700

    Finish BMKG data

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -17,16 +17,27 @@ def ekstraksi_data():
         content = requests.get('https://bmkg.go.id/')
     except:
         return None
-    if content.status_code == 200 :
-        soup = BeautifulSoup(content)
-        print()
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        time = soup.find('span', 'waktu').string.split(', ')
+        waktu = time[1]
+        tanggal = time[0]
+
+        result = soup.find('div', 'col-md-6 col-xs-6 gempabumi-detail no-padding')
+        magnitudo = result.findChildren('li')[1].text
+        kedalaman = result.findChildren('li')[2].text
+        koordinat = result.findChildren('li')[3].text.split(' - ')
+        print(koordinat)
+        pusat = result.findChildren('li')[4].text
+        dirasakan = result.findChildren('li')[5].text
         hasil = dict()
-        hasil['tanggal'] = '01 September 2021'
-        hasil['waktu'] = '14:50:35 WIB'
-        hasil['magnitudo'] = 5
-        hasil['lokasi'] = {'LS': 3.97, 'BT': 122.47}
-        hasil['pusat'] = 'berada di darat 0.5 km Tenggara Puuwatu, Kota Kendari'
-        hasil['dirasakan'] = '(Skala MMI): III-IV Kendari'
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['lokasi'] = {'LS': koordinat[0], 'BT': koordinat[1]}
+        hasil['pusat'] = pusat
+        hasil['dirasakan'] = dirasakan
 
         return hasil
     else:
@@ -41,9 +52,11 @@ def tampilkan_data(result):
     print(f'Tanggal : {result["tanggal"]}')
     print(f'Waktu : {result["waktu"]}')
     print(f'Magnitudo : {result["magnitudo"]} km')
+    print(f'Kedalaman : {result["kedalaman"]}')
     print(f'lokasi : LS -> {result["lokasi"]["LS"]} BT ->{result["lokasi"]["BT"]}')
     print(f'pusat gempa : {result["pusat"]}')
     print(f'dirasakan : {result["dirasakan"]}')
+
 
 if __name__ == '__main__':
     print('package gempa terkini')
