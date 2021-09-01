62f37edd9c240d2ae0119f347ef0fba24d41c701
AlfaRiza <malfariza45@gmail.com>
2021-09-01 21:35:42 +0700

    ADD bs4 and requests

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstraksi_data():
     """
     Tanggal : 01 September 2021,
@@ -8,18 +12,31 @@ def ekstraksi_data():
     Pusat gempa : berada di darat 0.5 km Tenggara Puuwatu, Kota Kendari
     Dirasakan : (Skala MMI): III-IV Kendari
     """
-    hasil = dict()
-    hasil['tanggal'] = '01 September 2021'
-    hasil['waktu'] = '14:50:35 WIB'
-    hasil['magnitudo'] = 5
-    hasil['lokasi'] = {'LS': 3.97, 'BT': 122.47}
-    hasil['pusat'] = 'berada di darat 0.5 km Tenggara Puuwatu, Kota Kendari'
-    hasil['dirasakan'] = '(Skala MMI): III-IV Kendari'
 
-    return hasil
+    try:
+        content = requests.get('https://bmkg.go.id/')
+    except:
+        return None
+    if content.status_code == 200 :
+        soup = BeautifulSoup(content)
+        print()
+        hasil = dict()
+        hasil['tanggal'] = '01 September 2021'
+        hasil['waktu'] = '14:50:35 WIB'
+        hasil['magnitudo'] = 5
+        hasil['lokasi'] = {'LS': 3.97, 'BT': 122.47}
+        hasil['pusat'] = 'berada di darat 0.5 km Tenggara Puuwatu, Kota Kendari'
+        hasil['dirasakan'] = '(Skala MMI): III-IV Kendari'
+
+        return hasil
+    else:
+        None
 
 
 def tampilkan_data(result):
+    if result is None:
+        print("Data tidak ada")
+        return
     print("Gempa terakhir berdasarkan BMKG")
     print(f'Tanggal : {result["tanggal"]}')
     print(f'Waktu : {result["waktu"]}')
